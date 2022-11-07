4f795167738d1b0050f11c1964a80f1088f46810
Sofia-Citrini <sofiacitrini@gmail.com>
2022-11-07 15:46:06 +0100

    adds bootstrap

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const prodotti = ["pane", "pasta", "latte", "caffe", "miele"];
+const prodotti = ["Pane", "Pasta", "Latte", "Caffe", "Miele"];
 console.log(prodotti);
 
 const listaSpesa = document.querySelector(".lista-spesa");
@@ -8,7 +8,7 @@ let i = 0;
 while (i < prodotti.length) {
     console.log(prodotti [i]);
 
-    listaSpesa.innerHTML += `<li>${prodotti [i]}</li>`;
+    listaSpesa.innerHTML += `<li class="list-group-item">${prodotti [i]}</li>`;
     
     i++;
 }
